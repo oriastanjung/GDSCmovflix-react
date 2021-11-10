f15414dd7345e2203d58fc6468598a1f8d27a855
Orias Tanjung <oriastanjung@users-MacBook-Pro.local>
2021-11-11 00:59:04 +0700

    Updates

---
+++ precache-manifest.4c96fd8d2c4927671395537cabc52be9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e387a09d17543323c7eece0da5630f28",
+    "url": "/GDSCmovflix-react/index.html"
+  },
+  {
+    "revision": "bf24de238a75f7fddcc3",
+    "url": "/GDSCmovflix-react/static/css/main.46566255.chunk.css"
+  },
+  {
+    "revision": "de9e265a838ee98100da",
+    "url": "/GDSCmovflix-react/static/js/2.62633439.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/GDSCmovflix-react/static/js/2.62633439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e6d10fdb99a1369478",
+    "url": "/GDSCmovflix-react/static/js/3.42c86680.chunk.js"
+  },
+  {
+    "revision": "bf24de238a75f7fddcc3",
+    "url": "/GDSCmovflix-react/static/js/main.a2dbc50e.chunk.js"
+  },
+  {
+    "revision": "71593a3cd9506f90633e",
+    "url": "/GDSCmovflix-react/static/js/runtime-main.8b1354a9.js"
+  },
+  {
+    "revision": "08424932dc8023a57b2351b359d42089",
+    "url": "/GDSCmovflix-react/static/media/film.08424932.svg"
+  },
+  {
+    "revision": "b0517f48de4c0b68034c90486d8bd7b6",
+    "url": "/GDSCmovflix-react/static/media/heroIMG.b0517f48.svg"
+  },
+  {
+    "revision": "e26354b1acc2c5c4d55338f9845dd896",
+    "url": "/GDSCmovflix-react/static/media/whislist.e26354b1.svg"
+  }
+]);
